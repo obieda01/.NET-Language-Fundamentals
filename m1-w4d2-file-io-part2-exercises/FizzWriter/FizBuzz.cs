8d74b763226a5e37a22cba55a77d20128413c043
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-02-08 09:36:09 -0500

    Final Solutions w4 day2 file IO part 2

--- m1-w4d2-file-io-part2-exercises/FizzWriter/FizBuzz.cs
+++ m1-w4d2-file-io-part2-exercises/FizzWriter/FizBuzz.cs
@@ -7,7 +7,7 @@ using System.IO;
 namespace FizzWriter
 {
     public static class FizzBuzz
-    {
+    {//C:\Users\aobiedat\Tech Elevator\.Net class\week1\ahmedobieda-c-exercises\FizzBuzz.txt
         public static void FizzBuzzWriter()
         {
             bool isValid = false;
@@ -15,50 +15,74 @@ namespace FizzWriter
             string destinationPath = string.Empty;
             while (!isValid)
             {
-                Console.WriteLine("Please input the Path you want the FizzBuzz.txt file to be on:");
-                destinationPath = Console.ReadLine();
-                if (!Directory.Exists(destinationPath))
+                try
                 {
-                    try
+                    Console.WriteLine("Please input the Path you want the FizzBuzz.txt file to be on:");
+                    destinationPath = Console.ReadLine();
+                    if (!Directory.Exists(destinationPath))
                     {
+
                         Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
                         File.Create(Path.GetFileName(destinationPath));
                         isValid = true;
                     }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("the Path you entered is NOT Valid");
-                    }
-                }
-                isValid = true;
-            }
 
-            using (StreamWriter sr = new StreamWriter(destinationPath))
-            {
-                for (int i = 1; i <= 300; i++)
-                {
-                    if ((i % 3 == 0 && i % 5 == 0) || (i.ToString().Contains("3") && !(i.ToString().Contains("5"))))
-                    {
-                        sr.WriteLine("Fizz ");
 
-                    }
-                    else
+                    isValid = true;
+
+
+                    using (StreamWriter sr = new StreamWriter(destinationPath))
                     {
-                        if ((i % 3 != 0 && i % 5 == 0) || (i.ToString().Contains("5") && !(i.ToString().Contains("3"))))
-                        {
-                            sr.WriteLine("Buzz ");
-                        }
-                        else
+                        for (int i = 1; i <= 300; i++)
                         {
                             if ((i % 3 == 0 && i % 5 == 0))
                             {
                                 sr.WriteLine("FizzBuzz ");
+
                             }
-                            else sr.WriteLine(i.ToString() + " ");
+                            else
+                            {
+                                if ((i % 3 != 0 && i % 5 == 0))
+                                {
+                                    sr.WriteLine("Buzz ");
+                                }
+                                else
+                                {
+                                    if ((i % 3 == 0 && i % 5 != 0))
+                                    {
+                                        sr.WriteLine("Fizz ");
+                                    }
+                                    else
+                                    {
+                                        if ((i.ToString().Contains("5") && !(i.ToString().Contains("3"))))
+                                        {
+                                            sr.WriteLine("Buzz");
+                                        }
+                                        else
+                                        {
+                                            if (i.ToString().Contains("3"))
+                                            {
+                                                sr.WriteLine("Fizz");
 
+                                            }
+                                            else sr.WriteLine(i.ToString());
+                                        }
+                                    }
+                                }
+
+                            }
                         }
-
                     }
+                }
+
+
+                catch (IOException e)
+                {
+                    Console.WriteLine("IO Exception :" + e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception :"+e.Message);
                 }
             }
         }
