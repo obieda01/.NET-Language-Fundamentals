906cc3d2a479bd01b08c388be6cb8a8e1b0420f4
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <alakmo8@gmail.com>
2017-02-13 09:31:00 -0500

    weekend independant practice

--- m1-w1d4-loops-arrays-exercises/Exercises/Lucky13.cs
+++ m1-w1d4-loops-arrays-exercises/Exercises/Lucky13.cs
@@ -8,7 +8,19 @@ namespace Exercises
 {
     public partial class Exercises
     {
-        /*
+        /*public  int[] findMaxMin(int[] sourceArray)
+        {
+            int maxValue = int.MinValue;
+            int minValue = int.MaxValue;
+            foreach (var item in sourceArray)
+            {
+                if (item < minValue) minValue = item;
+                if (item > maxValue) maxValue = item;
+            }
+            return new int[] { maxValue, minValue };
+            
+
+        }
          Given an array of ints, return true if the array contains no 1's and no 3's.
          lucky13([0, 2, 4]) → true
          lucky13([1, 2, 3]) → false
