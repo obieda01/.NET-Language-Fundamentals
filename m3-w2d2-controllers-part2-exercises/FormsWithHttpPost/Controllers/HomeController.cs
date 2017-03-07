d0ecff00f91686965a22266febae8a023f210418
John Fulton <john@TechElevator.com>
2017-03-07 08:27:32 -0500

    m3-w2d2-controllers-part2-exercises

---
+++ m3-w2d2-controllers-part2-exercises/FormsWithHttpPost/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using FormsWithHttpPost.DAL;
+using FormsWithHttpPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsWithHttpPost.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }        
+    }
+}
