e1e9bcdda06c51769bb698020190f3937b8b0a4e
Ahmad Obiedat <obiedatdeveloper@gmail.com>
2018-09-16 21:04:22 -0400

    resolve merge conflicts

--- m4-w1d2-history-geek-starter/HistoryGeek.Web/Scripts/exercises/dom-exercises.js
+++ m4-w1d2-history-geek-starter/HistoryGeek.Web/Scripts/exercises/dom-exercises.js
@@ -1,22 +1,18 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 //Locate all <div> elements and add the 'president' class.
 function presentialDivs() {
-<<<<<<< HEAD
+//<<<<<<< HEAD
     $("#presidents").children().filter("div").addClass("president");
 
-=======
-    $("#presidents div").addClass("president");
->>>>>>> db4216478a42a4c13166ccc88631c47333f917b0
+//=======
+//    $("#presidents div").addClass("president");
+//>>>>>>> db4216478a42a4c13166ccc88631c47333f917b0
 }
 
 //Locate George Washington and add the 'founding-father' class
 function georgeWashington() {
-<<<<<<< HEAD
     $("#george-washington").addClass("founding-father");
-=======
 
-
->>>>>>> db4216478a42a4c13166ccc88631c47333f917b0
 }
 
 //Locate all Republican presidents and add the 'red' class.
